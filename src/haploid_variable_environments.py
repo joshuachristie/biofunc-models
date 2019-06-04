2c963e679714742ff74443a379c858b7e041c1fc
Joshua Christie <joshua@pop-os.localdomain>
2019-06-04 10:16:55 +1000

    initial commit

---
+++ src/haploid_variable_environments.py
@@ -0,0 +1,8 @@
+""" 
+Starting with a basic model with the following properties:
+- haploid with two alleles (p and q)
+- two environmental types (E_p and E_q)
+- "infinite" number of environments with frequency P(E_p) and P(E_q)
+- variable fitness of each allele in each environment (p matched to E_p; q matched to E_q)
+- model can switch between deterministic and stochastic
+""" 
