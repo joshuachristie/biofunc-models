c5706cb6ea1d126f7af354d0dfa5af44703fc6d7
Joshua Christie <joshua.christie@sydney.edu.au>
2019-09-02 12:33:40 +1000

    header guards and add final declarations

--- src/simple_wright_fisher_haploid/functions.h
+++ src/simple_wright_fisher_haploid/functions.h
@@ -1,4 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include <vector>
+#include <random>
+
 std::vector<double> getFitnessFunction(const double selection_coefficient);
 void expectedAlleleFreqs(double &allele_A_freq, std::vector<double> &haploid_fitnesses);
 void realisedAlleleFreqs(double &allele_A_freq, const int population_size, std::mt19937 &rng);
+void iterateOverGenerations(double &allele_A_freq, const std::vector<double> haploid_fitnesses,
+			    const int population_size, const int number_generations);
+bool closeToValue(double allele_freq, double value);
 
+#endif
