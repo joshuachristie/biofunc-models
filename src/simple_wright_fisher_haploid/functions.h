a88b9e0500eaec52ebdbf3e0e32a9592b63b001a
Joshua Christie <joshua.christie@sydney.edu.au>
2019-09-02 12:01:06 +1000

    update header

--- src/simple_wright_fisher_haploid/functions.h
+++ src/simple_wright_fisher_haploid/functions.h
@@ -1,1 +1,4 @@
 std::vector<double> getFitnessFunction(const double selection_coefficient);
+void expectedAlleleFreqs(double &allele_A_freq, std::vector<double> &haploid_fitnesses);
+void realisedAlleleFreqs(double &allele_A_freq, const int population_size, std::mt19937 &rng);
+
