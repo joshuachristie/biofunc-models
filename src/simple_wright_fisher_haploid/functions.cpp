ed838078c68979aac756a4736f4a7450da50a2fb
Joshua Christie <joshua.christie@sydney.edu.au>
2019-09-02 11:55:35 +1000

    funtions for simple haploid wright-fisher

---
+++ src/simple_wright_fisher_haploid/functions.cpp
@@ -0,0 +1,22 @@
+std::vector<double> getFitnessFunction(const double selection_coefficient){
+  // haploid_fitnesses[w_A, w_a] gives relative fitness of A allele
+  std::vector<double> haploid_fitnesses {1.0 + selection_coefficient, 1.0};
+  return haploid_fitnesses
+}
+
+void expectedAlleleFreqs(double &allele_A_freq, std::vector<double> &haploid_fitnesses){
+  // expected frequency of allele A after selection
+  std::vector<double> expected_allele_freq_raw(2);
+  expected_allele_freq_raw[0] = allele_A_freq * haploid_fitnesses[0];
+  expected_allele_freq_raw[1] = (1.0 - allele_A_freq) * haploid_fitnesses[1];
+  // replace allele_A_freq with normalised expectation
+  allele_A_freq = expected_allele_freq_raw[0] / (std::accumulate(expected_allele_freq_raw.begin(),
+								 expected_allele_freq_raw.end(), 0.0));
+}
+
+void realisedAlleleFreqs(double &allele_A_freq, const int population_size){
+  // realised frequency of allele A after selection
+ std:binomial_distribution<int> surviving_As(population_size, allele_A_freq); // use expectation of allele A
+  allele_A_freq = static_cast<double>(surviving_As(rng)) / static_cast<double>(population_size); // proportion
+}
+
