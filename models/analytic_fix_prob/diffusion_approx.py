da5f3b6d1071f6019f4d8bbca9b3a2b752424c15
Joshua Christie <joshua.christie@sydney.edu.au>
2019-09-26 18:25:31 +1000

    remove misplaced, redundant code
    equivalent will be in the script that compares fixation prob between
    simulation and analytic approx

--- models/analytic_fix_prob/diffusion_approx.py
+++ models/analytic_fix_prob/diffusion_approx.py
@@ -5,4 +5,3 @@ def haploid_fix_prob(eff_pop_size, select_coef, initial_freq):
     dem = 1 - np.exp(-2 * eff_pop_size * select_coef, dtype=np.float128)
     return num / dem
 
-# fix_prob = np.array([fixation_probability(1, N, s, 1/N) for s in np.linspace(-1,1,998)])
