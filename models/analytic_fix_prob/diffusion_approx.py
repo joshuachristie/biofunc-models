cf23e4d7ef86e616a9ce22295311ec73b47d74ef
Joshua Christie <joshua.christie@sydney.edu.au>
2019-09-26 18:22:49 +1000

    tidy up diffusion approximation

--- models/analytic_fix_prob/diffusion_approx.py
+++ models/analytic_fix_prob/diffusion_approx.py
@@ -1,10 +1,8 @@
 import numpy as np
 
-def fixation_probability(ploidy_level, effective_pop_size,
-                         selection_coefficient, frequency):
-    return (1 - np.exp(-2 * ploidy_level * effective_pop_size *
-                          selection_coefficient * frequency, dtype=np.float128)) / (
-                              1 - np.exp(-2 * ploidy_level * effective_pop_size *
-                                            selection_coefficient,dtype=np.float128))
+def haploid_fix_prob(eff_pop_size, select_coef, initial_freq):
+    num = 1 - np.exp(-2 * eff_pop_size * select_coef * initial_freq, dtype=np.float128)
+    dem = 1 - np.exp(-2 * eff_pop_size * select_coef, dtype=np.float128)
+    return num / dem
 
-fix_prob = np.array([fixation_probability(1, N, s, 1/N) for s in np.linspace(-1,1,998)])
+# fix_prob = np.array([fixation_probability(1, N, s, 1/N) for s in np.linspace(-1,1,998)])
