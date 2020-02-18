c8dbc2d276f18ad16a049829aa6cb752b4993975
Joshua Christie <joshua.christie@sydney.edu.au>
2020-02-19 10:36:06 +1100

    change fix prob so it can be used for haploid/diploid

--- models/analytic_fix_prob/diffusion_approx.py
+++ models/analytic_fix_prob/diffusion_approx.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-def haploid_fix_prob(eff_pop_size, select_coef, initial_freq):
-    num = 1 - np.exp(-2 * eff_pop_size * select_coef * initial_freq, dtype=np.float128)
-    dem = 1 - np.exp(-2 * eff_pop_size * select_coef, dtype=np.float128)
+def fix_prob(eff_pop_size, select_coef, ploidy):
+    num = 1 - np.exp(-2 * (ploidy * eff_pop_size) * select_coef * (1 / (ploidy * eff_pop_size)), dtype=np.float128)
+    dem = 1 - np.exp(-2 * (ploidy * eff_pop_size) * select_coef, dtype=np.float128)
     return num / dem
-
