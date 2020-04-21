024e7f024d33ab7198c86f8307d531eb3a2da4e0
Joshua Christie <joshua.christie@sydney.edu.au>
2020-04-21 15:19:51 +1000

    add tolerance and update includes for haploid two effects

--- models/wright_fisher/haploid_two_effects_one_environment/functions.h
+++ models/wright_fisher/haploid_two_effects_one_environment/functions.h
@@ -15,8 +15,7 @@ void expectedAlleleFreqs(double &allele_A_freq, const std::vector<double> &haplo
 void realisedAlleleFreqs(double &allele_A_freq, const int population_size, std::mt19937 &rng);
 // iterate through generations until one allele is fixed or the max number of generations is reached
 void iterateOverGenerations(double &allele_A_freq, const std::vector<double> &haploid_fitnesses,
-			    const int population_size, const int number_generations, std::mt19937 &rng);
-// returns true if allele_freq is approx equal to value
-bool closeToValue(double allele_freq, double value);
+			    const int population_size, const int number_generations, std::mt19937 &rng,
+			    const double tolerance);
 
 #endif
