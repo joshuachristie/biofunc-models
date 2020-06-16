d2c7f84da24f693ea94ed533105d6fb94836206f
Joshua Christie <joshua.christie@sydney.edu.au>
2020-06-16 10:19:55 +1000

    combine PID and calculate_metric and print data

--- analysis/metric_calculation/process_persistence_probs_to_metric.m
+++ analysis/metric_calculation/process_persistence_probs_to_metric.m
@@ -5,10 +5,10 @@
 % for all models but haploid_single_environment, it calls
 % /analysis/PID/redAsMinIComponentTotal.m, which performs partial
 % information decomposition.
-% Processes entire set of persistence probabilities from 
-% /data/persistence_probabilities and prints data to 
+% Processes entire set of persistence probabilities from
+% /data/persistence_probabilities and prints data to
 % /data/calculated_metric_values
-
+addpath('../PID');
 subdirs = dir('../../data/persistence_probabilities');
 subdirs = subdirs(~ismember({subdirs.name}, {'.', '..'}));
 
@@ -18,10 +18,6 @@ for i = 1:numel(subdirs)
         list_of_files = dir(path_to_folder);
         list_of_files = list_of_files(~ismember({list_of_files.name}, {'.', '..'}));
         
-        for j = 1:numel(list_of_files)            
-            persistence_probs = csvread(strcat(path_to_folder, '/', list_of_files(j).name)); 
-        end
-            
         % csv has four floats and contains persistence probabilities according to the following scheme (0-based indexing):
         
         % DSE
@@ -29,13 +25,13 @@ for i = 1:numel(subdirs)
         % 1st element: heterozygote effect alone
         % 2nd element: homozygote effect alone
         % 3rd element: focal situation (effect of both heterozygote and homozygote)
-
+        
         % HTEOE
         % 0th element: neutral drift
         % 1st element: selection_coefficient_A2 alone
         % 2nd element: selection_coefficient_A1 alone
         % 3rd element: focal situation (effect of both selection_coefficient_A1 and selection_coefficient_A2)
-
+        
         % HTE
         % 0th element: neutral drift
         % 1st element: selection_coefficient_A2 alone
@@ -44,17 +40,27 @@ for i = 1:numel(subdirs)
         
         % source 1 refers to the homozygote (DSE) and A1 (HTEOE/HTE)
         % source 2 refers to the heterozygote (DSE) and A2 (HTEOE/HTE)
-
-        pTable = [
-            0, 0, 1, persistence_probs(1) / 4; 0, 0, 0, (1-persistence_probs(1)) / 4;...
-            0, 1, 1, persistence_probs(2) / 4; 0, 1, 0, (1-persistence_probs(2)) / 4;...
-            1, 0, 1, persistence_probs(3) / 4; 1, 0, 0, (1-persistence_probs(3)) / 4;...
-            1, 1, 1, persistence_probs(4) / 4; 1, 1, 0, (1-persistence_probs(4)) / 4
-            ];
         
-        % call redAsMinIComponentTotal(pTable) to get normalised PID scores
-       % call function to calculate metric
-       % weight metric by PID
+        % calculate overall metric (non-decomposed)
+        for j = 1:numel(list_of_files)
+            persistence_probs = csvread(strcat(path_to_folder, '/', list_of_files(j).name));
+            % calculate_metric takes prob due to drift and due to focal situation
+            metric = calculate_metric(persistence_probs(1), persistence_probs(4));
+            % get normalised weighted PID (R, U1, U2, C)
+            pTable = [
+                0, 0, 1, persistence_probs(1) / 4; 0, 0, 0, (1-persistence_probs(1)) / 4;...
+                0, 1, 1, persistence_probs(2) / 4; 0, 1, 0, (1-persistence_probs(2)) / 4;...
+                1, 0, 1, persistence_probs(3) / 4; 1, 0, 0, (1-persistence_probs(3)) / 4;...
+                1, 1, 1, persistence_probs(4) / 4; 1, 1, 0, (1-persistence_probs(4)) / 4
+                ];
+
+            normalised_PID_both_sources_present = redAsMinIComponentTotal(pTable);
+            % get decomposed metric and write to data/
+            metric_decomposition = metric * normalised_PID_both_sources_present;
+            csvwrite(sprintf('../../data/calculated_metric_values/%s/%s', subdirs(i).name, ...
+                regexprep(list_of_files(j).name, '.csv', '_metric_decomposition.csv')), metric_decomposition);
+        end
+        
     else
         % HSE
         % csv has two floats (doesn't require PID)
@@ -65,15 +71,14 @@ for i = 1:numel(subdirs)
         list_of_files = dir(path_to_folder);
         list_of_files = list_of_files(~ismember({list_of_files.name}, {'.', '..'}));
         
-        % calculate metric and print to file
-        for j = 1:numel(list_of_files)            
-            persistence_probs = csvread(strcat(path_to_folder, '/', list_of_files(j).name)); 
+        % calculate metric and write to data/
+        for j = 1:numel(list_of_files)
+            persistence_probs = csvread(strcat(path_to_folder, '/', list_of_files(j).name));
             metric = calculate_metric(persistence_probs(1), persistence_probs(2));
             csvwrite(sprintf('../../data/calculated_metric_values/%s/%s', subdirs(i).name, ...
                 regexprep(list_of_files(j).name, '.csv', '_metric.csv')), metric);
         end
-
+        
     end
 end
 
-        
