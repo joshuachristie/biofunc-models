ba21e5546664e197e294a8f1306c93cda1d94028
Joshua Christie <joshua@pop-os.localdomain>
2020-08-14 15:54:33 +1000

    rename (and move) helper_functions, document and name consistently

---
+++ population_genetics_models/helper_functions.cpp
@@ -0,0 +1,21 @@
+/**
+   @file helper_functions.cpp
+   @brief Collection of miscellaneous functions
+*/
+#include <cmath>
+#include "helper_functions.h"
+
+/**
+   @brief Compare for approximate equality of doubles
+   @param[in] allele_freq Allele frequency
+   @param[in] value Value to compare \p allele_freq against
+   @param[in] tolerance A double that determines how closely \p allele_freq and \p value must be
+   @return True if the absolute difference between \p allele_freq and \p value is less than \p tolerance; false otherwise
+*/
+bool close_to_value(const double allele_freq, const double value, const double tolerance){
+  if (std::abs(allele_freq - value) < tolerance){
+    return 1;
+  } else {
+    return 0;
+  }
+}
--- population_genetics_models/include/helper_functions.h
+++
@@ -1,7 +0,0 @@
-// provides closeToValue(), which tests whether for equality of a double
-#ifndef HELPER_FUNCTIONS_H
-#define HELPER_FUNCTIONS_H
-
-bool closeToValue(double allele_freq, double value, double tolerance);
-
-#endif 
