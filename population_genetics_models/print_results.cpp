a284c5a1de2a96c3203bb5fa286e1b887251a085
Joshua Christie <joshua@pop-os.localdomain>
2020-08-20 12:51:10 +1000

    add documentation and remove redundant code

--- population_genetics_models/print_results.cpp
+++ population_genetics_models/print_results.cpp
@@ -9,40 +9,16 @@
 #include "print_results.h"
 
 namespace print {
-    /**
+  /**
      @brief Prints persistence probability to file
+     @param[in] argc Number of command line arguments
+     @param[in] argv Array of command line arguments
+     @param[in] persistence_probability Probability that the allele persists in population
   */
   void print_persistence_probability(int argc, char* argv[], const double persistence_probability){
-    // make directory
-    // std::string parent_directory = "./data/";
-    // std::ostringstream dir_path;
-    // dir_path << parent_directory << argv[1] << "/";
-    // std::filesystem::create_directories(dir_path.str());
-    // // generate filename for csv
-    // std::ostringstream filename (dir_path.str(), std::ios_base::ate);
-    // for (int i = 2; i < argc - 1; i++){
-    //   filename << argv[i] << "_";
-    // }
-    // filename << argv[argc -1] << ".csv";
     std::ostringstream filename = create_dir_and_get_filename(argc, argv);
     write_value_to_file(persistence_probability, filename);
-
-
-    
-    // check whether file is empty
-    // std::ifstream infile (filename.str());
-    // if (is_empty(infile)){ // if so, no comma
-    //   infile.close();
-    //   std::ofstream outfile (filename.str(), std::ofstream::app);
-    //   outfile << allele_A_freq;
-    // } else { // if not empty, add comma before value
-    //   infile.close();
-    //   std::ofstream outfile (filename.str(), std::ofstream::app);
-    //   outfile << "," << allele_A_freq;
-    // }
   }
-
-  
   /**
      @brief Checks whether file is empty
      @param[in] infile File to check
@@ -51,7 +27,12 @@ namespace print {
   bool is_empty(std::ifstream& infile){
     return infile.peek() == std::ifstream::traits_type::eof();
   }
-
+  /**
+     @brief Creates directory for results and returns filename
+     @param[in] argc Number of command line arguments
+     @param[in] argv Array of command line arguments
+     @return filename \p std::ostringstream with path to csv file
+*/
   std::ostringstream create_dir_and_get_filename(int argc, char* argv[]){
     std::string parent_directory = "../data/";
     std::ostringstream dir_path;
--- population_genetics_models/print_results.h
+++ population_genetics_models/print_results.h
@@ -1,15 +1,25 @@
+/**
+   @file print_results.h
+*/
 #ifndef PRINT_RESULTS_H
 #define PRINT_RESULTS_H
 
 #include <fstream>
 #include <sstream>
 
+/** Namespace for print methods*/
 namespace print {
   
   void print_persistence_probability(int argc, char* argv[], const double allele_A_freq);
   bool is_empty(std::ifstream& infile);
   std::ostringstream create_dir_and_get_filename(int argc, char* argv[]);
 
+  /**
+     @brief Template method that writes a value to file
+     @param[in] value_to_write The template-type value to write to file
+     @param[in] filename Name of file to write to
+     @return Nothing (but writes \p value_to_write to \p filename)
+*/
   template <class T>
   void write_value_to_file(const T value_to_write, std::ostringstream &filename){
     // check whether file is empty
