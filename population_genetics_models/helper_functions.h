e01045f235f2dc27d76e2b784fb8482b5a09ddb2
Joshua Christie <joshua@pop-os.localdomain>
2020-08-27 15:52:01 +1000

    add is_not_extinct() method
    for issue #23

--- population_genetics_models/helper_functions.h
+++ population_genetics_models/helper_functions.h
@@ -14,13 +14,23 @@ namespace help {
      @param[in] params.fixed.max_generations_per_sim Maximum number of generations for which a sim can run (approximation to time -> infinity)
      @param[in] params.fixed.tolerance Tolerance for comparison of doubles
      @return True if allele A is neither fixed nor extinct (nor has exceeded params.fixed.max_generations_per_sim); false otherwise
-*/
+  */
   template<class T>
   bool is_neither_fixed_nor_extinct(const double gen, const double value, const T &params){
     return gen < params.fixed.max_generations_per_sim &&
       !(
 	close_to_value(value, 0.0, params.fixed.tolerance) || close_to_value(value, 1.0, params.fixed.tolerance)
 	);
+  }
+  /**
+     @brief Checks whether \p value > 0
+     @param[in] value Allele frequency
+     @param[in] params.fixed.tolerance Tolerance for comparison of doubles
+     @return True if \p value is > 0; false otherwise
+  */
+  template<class T>
+  bool is_not_extinct(const double value, const T &params){
+    return !(close_to_value(value, 0.0, params.fixed.tolerance) || value < 0.0);
   }
 
 }
