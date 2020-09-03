9514a68d92ce57b6787c6f073631d30c120ee94a
Joshua Christie <joshua@pop-os.localdomain>
2020-09-03 12:32:26 +1000

    update paths to data directories

--- population_genetics_models/path_parameters.h
+++ population_genetics_models/path_parameters.h
@@ -5,7 +5,8 @@
 
 namespace paths {
 
-  inline constexpr std::string_view persistence_data_directory = "../data/persistence_probability/";
+  inline constexpr std::string_view persistence_infinite_data_dir = "../data/persistence_probability/infinite_approximation/";
+    inline constexpr std::string_view persistence_finite_data_dir = "../data/persistence_probability/finite_generations/";
   inline constexpr std::string_view error_file_directory = "../run/error_logs/";
 
 }
