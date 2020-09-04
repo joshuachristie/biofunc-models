3107d42752604e3d9fcb4fd116d41a51dc2cea09
Joshua Christie <joshua@pop-os.localdomain>
2020-09-04 12:33:57 +1000

    add definitions and outline of implementation for Data classes

---
+++ population_genetics_models/Data.h
@@ -0,0 +1,38 @@
+#ifndef DATA_H
+#define DATA_H
+
+#include <vector>
+
+class DataPersistenceInfiniteApprox {
+private:
+  bool _persistence;
+public:
+  void set_infinite_approx_persistence(bool);
+  void print_infinite_approx_persistence();
+  /** constructors */
+  DataPersistenceInfiniteApprox() {}
+  DataPersistenceInfiniteApprox(bool persistence) : _persistence{persistence} {}
+};
+
+class DataPersistenceMultipleGens : public DataPersistenceInfiniteApprox {
+private:
+  std::vector<bool> _persistence_by_gen;
+public:
+  void append_persistence_by_gen(bool);
+  void print_persistence_by_gen();
+  /** constructor */
+  DataPersistenceMultipleGens(int reserve_length_mg) { _persistence_by_gen.reserve(reserve_length_mg); }
+};
+
+class DataAlleleFreq : public DataPersistenceMultipleGens {
+private:
+  std::vector<double> _allele_A_freq_by_gen;
+public:
+  void append_allele_A_freq_by_gen(double);
+  void print_allele_A_freq_by_gen();
+  /** constructor */
+  DataAlleleFreq(int reserve_length_mg, int reserve_length_af) :
+    DataPersistenceMultipleGens {reserve_length_mg} { _allele_A_freq_by_gen.reserve(reserve_length_af); }
+};
+
+#endif
