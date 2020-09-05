0857c76ef70d5e6b515963d00a32f4e97faa00ce
Joshua Christie <joshua@pop-os.localdomain>
2020-09-06 09:57:01 +1000

    implement framework with multiple inheritance and virtual functions

--- population_genetics_models/DataContainers.h
+++ population_genetics_models/DataContainers.h
@@ -4,35 +4,13 @@
 #include <vector>
 #include "Data.h"
 
-// keep in mind that it might be possible to make it cleaner by inheriting from templated DataContainer
-// but it might only be the one line with _simulation_data so maybe more work than worth
-// but whether I inherit from a base template class, i will want to be doing some inheritance from the
-// specialised classes as they build on each other (e.g. I need to be able to print the infinite approx fixation
-// from all three classes)
-
-
-// also just a note that it is probably worth having some generic print_results() member function for all of the
-// classes but for it to be polymorphic (for DataPersistenceInfiniteApprox it just prints the infinite approx, for DataPersistenceMultipleGens it prints the infinite approx (which will be inherited) and the multiple gen print (which will be a member function of DataPersistenceMultipleGens; for DataAlleleFreq it prints the infinite approx (inherited from DataPersistenceInfiniteApprox), multiple gen (inherited from DataPersistenceMultipleGens), and the raw data (member function of DataAlleleFreq)
-
-// actually changed my mind on this - I don't think I should be printing from within the member functions at all
-// I can keep some (simplified) free print functions but the member functions here should just be returning the
-// data 
-
-template <class, int number_replicates>
-class DataContainer {};
-
-template <int number_replicates>
-class DataContainer <DataPersistenceInfinite, number_replicates> {
-// private:
-//   std::vector<DataPersistenceInfinite> _simulation_data;
+template <class T>
+class DataContainer_Base_Infinite_Approx {
 public:
-  std::vector<DataPersistenceInfinite> _simulation_data;
-  DataContainer(){
-    for (int i = 0; i < number_replicates; i++){
-      _simulation_data.push_back(DataPersistenceInfinite());
-    }
-  }
-  const double get_persistence_infinite_approx(){
+  
+  std::vector<T> _simulation_data;
+  
+  virtual const double get_persistence_infinite_approx(){
     int persistence_count = 0;
     for (auto it = _simulation_data.begin(); it != _simulation_data.end(); it++){
       persistence_count += it->_persistence;
@@ -42,52 +20,84 @@ public:
 
 };
 
-template <int number_replicates>
-class DataContainer <DataPersistenceByGen, number_replicates> {
-// private:
-//   std::vector<DataPersistenceByGen> _simulation_data;
+template <class T, int number_replicates>
+class DataContainer_Base_Persistence_By_Gen : public virtual DataContainer_Base_Infinite_Approx<T>
+{
 public:
-  std::vector<DataPersistenceByGen> _simulation_data;
-  DataContainer(int reserve_length_mg){
-    for (int i = 0; i < number_replicates; i++){
-      _simulation_data.push_back(DataPersistenceByGen(reserve_length_mg));
-    }
-  }
   
-  const std::vector<double> get_persistence_by_gen(){
-    
-    std::vector<double> persistence_probability(_simulation_data.size(), 0.0);
+  virtual const std::vector<double> get_persistence_by_gen(){
+    std::vector<double> persistence_probability(this->_simulation_data.size(), 0.0);
     for (int i = 0; i < number_replicates; i++){
       for (int j = 0; j < persistence_probability.size(); j++){
-	persistence_probability[j] += (static_cast<double>(_simulation_data[i]._persistence_by_gen[j]) /
+	persistence_probability[j] += (static_cast<double>(this->_simulation_data[i]._persistence_by_gen[j]) /
 				       static_cast<double>(number_replicates));
       }
     }
     return persistence_probability;
   }
+ 
+};
+
+template <class T>
+class DataContainer_Base_Allele_A_Freq : public virtual  DataContainer_Base_Infinite_Approx<T> {
+public:
+  const double get_allele_A_freqs(int replicate, int gen){
+    return this->_simulation_data[replicate]._allele_A_freq_by_gen[gen];
+  }
+};
+
+
+
+template <class T, int number_replicates>
+class DataContainer : public DataContainer_Base_Infinite_Approx<T> {};
+
+template <int number_replicates>
+class DataContainer <DataPersistenceInfinite, number_replicates> : public
+DataContainer_Base_Infinite_Approx<DataPersistenceInfinite> {
+public:
+  // constructor
+  DataContainer(){
+    for (int i = 0; i < number_replicates; i++){
+      this->_simulation_data.push_back(DataPersistenceInfinite());
+    }
+  }
+
+};
+
+template <int number_replicates>
+class DataContainer <DataPersistenceByGen, number_replicates> :
+  public DataContainer_Base_Persistence_By_Gen<DataPersistenceByGen, number_replicates> {
+public:
+  // constructor
+  DataContainer(int reserve_length_mg){
+    for (int i = 0; i < number_replicates; i++){
+      this->_simulation_data.push_back(DataPersistenceByGen(reserve_length_mg));
+    }
+  }
   
 };
 
 template <int number_replicates>
-class DataContainer <DataAlleleFreq, number_replicates> {
-// private:
-//   std::vector<DataAlleleFreq> _simulation_data;
+class DataContainer <DataAlleleFreq, number_replicates> :
+  public DataContainer_Base_Persistence_By_Gen<DataAlleleFreq, number_replicates>, public DataContainer_Base_Allele_A_Freq<DataAlleleFreq> {
 public:
-  std::vector<DataAlleleFreq> _simulation_data;
+  // constructor for infinite approx and to output raw allele data
+  // DataContainer(int reserve_length_af){
+  //   for (int i = 0; i < number_replicates; i++){
+  //     this->_simulation_data.push_back(DataAlleleFreq(reserve_length_af));
+  //   }
+  // }
+  // constructor for storing (in addition to infinite approx) both raw allele data and persistence_prob by gen
   DataContainer(int reserve_length_mg, int reserve_length_af){
     for (int i = 0; i < number_replicates; i++){
-      _simulation_data.push_back(DataAlleleFreq(reserve_length_mg, reserve_length_af));
-    }
-  }
-  DataContainer(int reserve_length_af){
-    for (int i = 0; i < number_replicates; i++){
-      _simulation_data.push_back(DataAlleleFreq(reserve_length_af));
+      this->_simulation_data.push_back(DataAlleleFreq(reserve_length_mg, reserve_length_af));
     }
   }
 
   double get_allele_A_freqs(int replicate, int gen){
-    return _simulation_data[replicate]._allele_A_freq_by_gen[gen];
+    return this->_simulation_data[replicate]._allele_A_freq_by_gen[gen];
   }
+  
 };
   
 #endif
