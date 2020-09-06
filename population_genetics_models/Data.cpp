e8e7ffc05830ed4afb2629d415344192b88ab28a
Joshua Christie <joshua@pop-os.localdomain>
2020-09-06 12:22:11 +1000

    split DataAlleleFreq into two classes
    one with and one without the members to record persistence data each
    generation. removed the default constructor for DataPersistenceByGen
    since it will never be called (it was just there so that I could
    construct DataAlleleFreq without the persistence data members but I'm
    now separating out these functions into two separate
    classes (DataAlleleFreq and DataAlleleFreqAndPBG)

--- population_genetics_models/Data.cpp
+++ population_genetics_models/Data.cpp
@@ -8,3 +8,7 @@ void DataPersistenceByGen::append_persistence_by_gen(bool persistence){
 void DataAlleleFreq::append_allele_A_freq_by_gen(double allele_A_freq){
   _allele_A_freq_by_gen.push_back(allele_A_freq);
 }
+
+void DataAlleleFreqAndPGB::append_allele_A_freq_by_gen(double allele_A_freq){
+  _allele_A_freq_by_gen.push_back(allele_A_freq);
+}
--- population_genetics_models/Data.h
+++ population_genetics_models/Data.h
@@ -15,18 +15,24 @@ class DataPersistenceByGen : public DataPersistenceInfinite {
 public:
   std::vector<bool> _persistence_by_gen;
   void append_persistence_by_gen(bool);
-  /** constructors */
-  DataPersistenceByGen() = default;
+  /** constructor */
   DataPersistenceByGen(int reserve_length_mg) { _persistence_by_gen.reserve(reserve_length_mg); }
 };
 
-class DataAlleleFreq : public DataPersistenceByGen {
+class DataAlleleFreq : public DataPersistenceInfinite {
 public:
   std::vector<double> _allele_A_freq_by_gen;
   void append_allele_A_freq_by_gen(double);
-  /** constructors */
+  /** constructor */
   DataAlleleFreq(int reserve_length_af) { _allele_A_freq_by_gen.reserve(reserve_length_af); }
-  DataAlleleFreq(int reserve_length_mg, int reserve_length_af) :
+};
+
+class DataAlleleFreqAndPBG : public DataPersistenceByGen {
+public:
+  std::vector<double> _allele_A_freq_by_gen;
+  void append_allele_A_freq_by_gen(double);
+  /** constructor */
+  DataAlleleFreqAndPBG(int reserve_length_mg, int reserve_length_af) :
     DataPersistenceByGen {reserve_length_mg} { _allele_A_freq_by_gen.reserve(reserve_length_af); }
 };
 
