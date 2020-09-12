14e8796f9d28e770b23ef9935424b55ad3941080
Joshua Christie <joshua@pop-os.localdomain>
2020-09-12 15:52:40 +1000

    add documentation for Parameters.h and change dir structure

--- population_genetics_models/Parameters.h
+++ population_genetics_models/Parameters.h
@@ -1,6 +1,6 @@
 /**
    @file
-   @brief Definitions for parameter structs
+   @brief Stucts that store parameter values for the model
 */
 
 #ifndef PARAMETERS_H
@@ -9,35 +9,36 @@
 #include "fixed_parameters.h"
 
 /**
-   @brief Struct containing shared parameters (common to all models)
+   @brief Struct containing shared parameters (i.e. parameters utilised by all of the population genetics models)
 */
 struct Shared_Parameters {
-  const int population_size;
-  const double initial_A_freq;
-  const int number_reinvasions;
+  const int population_size; /**< Number of alleles in the population */
+  const double initial_A_freq; /**< Initial frequency of allele A  */
+  const int number_reinvasions; /**< Number of reinvasion attempts that allele A faces */
+  /** Sets the number of generations for which to calculate and print time-dependent persistence_probability */
   const int number_gens_to_output_pp;
-  const bool print_allele_A_raw_data;
+  const bool print_allele_A_raw_data; /**< If true, will print raw allele data for each gen and replicate */
 };
 /**
    @brief Struct containing constant parameter values (fixed for all models)
 */
 struct Fixed_Parameters {
-  inline static const double tolerance = fixed_parameters::tolerance;
+  inline static const double tolerance = fixed_parameters::tolerance; /**< Tolerance for comparing doubles */
+   /** Number of independent stochastic replicates that are run */
   inline static const int number_replicates = fixed_parameters::number_replicates;
+  /** Max number of generations to run a replicate (prevents a stable polymorphism causing an infinite loop) */
   inline static const int max_generations_per_sim = fixed_parameters::max_generations_per_sim;
+  /** Memory allocation for Data::_allele_A_freq_by_gen */
   inline static const int reserve_memory_allele_freq = fixed_parameters::reserve_memory_allele_freq;
 };
 /**
    @brief Struct for parameters of the Haploid Single Environment model
 */
 struct HSE_Model_Parameters {
-  // add struct containing shared parameters
   Shared_Parameters shared;
-  // add struct for HSE-specific parameters
   struct HSE_Specific_Parameters {
-    const double selection_coefficient;
+    const double selection_coefficient; /**< Selection coefficient of allele A */
   } model;
-  // add struct containing fixed parameter values
   Fixed_Parameters fixed;
 };
 
@@ -45,31 +46,25 @@ struct HSE_Model_Parameters {
    @brief Struct for parameters of the Diploid Single Environment model
 */
 struct DSE_Model_Parameters {
-  // add struct containing shared parameters
   Shared_Parameters shared;
-  // add struct for DSE-specific parameters
   struct DSE_Specific_Parameters {
-    const double selection_coefficient_homozygote;
-    const double selection_coefficient_heterozygote;
+    const double selection_coefficient_homozygote; /**< Selection coefficient of the AA genotype */
+    const double selection_coefficient_heterozygote; /**< Selection coefficient of the Aa genotype */
   } model;
-  // add struct containing fixed parameter values
   Fixed_Parameters fixed;
 };
 /**
    @brief Struct for parameters of the Haploid Two Environments model
 */
 struct HTE_Model_Parameters {
-  // add struct containing shared parameters
   Shared_Parameters shared;
-  // add struct for HTE-specific parameters
   struct HTE_Specific_Parameters {
-    const double selection_coefficient_A_env_1;
-    const double selection_coefficient_A_env_2;
-    const double selection_coefficient_a_env_1;
-    const double selection_coefficient_a_env_2;
-    const int gen_env_1;
+    const double selection_coefficient_A_env_1; /**< Selection coefficient of the A allele in environment 1 */
+    const double selection_coefficient_A_env_2; /**< Selection coefficient of the A allele in environment 2 */
+    const double selection_coefficient_a_env_1; /**< Selection coefficient of the a allele in environment 1 */
+    const double selection_coefficient_a_env_2; /**< Selection coefficient of the a allele in environment 2 */
+    const int gen_env_1; /**< Number of generations spent in environment 1 (the initial environment) */
   } model;
-  // add struct containing fixed parameter values
   Fixed_Parameters fixed;
 };
 
@@ -77,16 +72,13 @@ struct HTE_Model_Parameters {
    @brief Struct for parameters of the Haploid Two Effects One Environment model
 */
 struct HTEOE_Model_Parameters {
-  // add struct containing shared parameters
   Shared_Parameters shared;
-  // add struct for HTEOE-specific parameters
   struct HTEOE_Specific_Parameters {
-    const double selection_coefficient_A1;
-    const double selection_coefficient_A2;
-    const double selection_coefficient_a1;
-    const double selection_coefficient_a2;
+    const double selection_coefficient_A1; /**< Selection coefficient of the A allele's 1st effect */
+    const double selection_coefficient_A2; /**< Selection coefficient of the A allele's 2nd effect */
+    const double selection_coefficient_a1; /**< Selection coefficient of the a allele's 1st effect */
+    const double selection_coefficient_a2; /**< Selection coefficient of the a allele's 2nd effect */
   } model;
-  // add struct containing fixed parameter values
   Fixed_Parameters fixed;
 };
 
